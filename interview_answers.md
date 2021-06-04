0494b269fcd796fa9a9b51c12321b3080d5baaab
escttsalo <escttsalo@gmail.com>
2021-06-04 15:59:41 -0600

    finish questions and add tests

--- interview_answers.md
+++ interview_answers.md
@@ -2,9 +2,14 @@
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. These will not be counted as a part of your sprint score but will be helpful for preparing you for your endorsement interview, and enhancing overall understanding.
 
 1. What are the main differences between a stateful and a functional component?
+    -Stateful component deals with anything stateful while a functional focuses on being functional.
 
 2. When does a componentWillMount function be called? What about a componentWillUpdate?
+    -Called right before mounting happens. 
+    -Call update when new props or state are received.
 
 3. Define stateful logic.
+    -Any logic having to do with the state of a component.
 
 4. What are the three step of creating a successful test? What is done in each phase?
+    -Arrange, Act, Assert
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+
+    const submit = screen.getByRole('button')
+    userEvent.click(submit);
+
+    await waitFor(() => {
+        const success = screen.queryByTestId(/successMessage/i);
+
+        expect(success).toBeInTheDocument();
+    })
+});
